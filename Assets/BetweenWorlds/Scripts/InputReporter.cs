9164bf27af7e3ad51b37b4c51b642046d19c4922
voxon-BW <38233064+voxon-BW@users.noreply.github.com>
2022-05-18 16:22:51 +0930

    Initial Commit

---
+++ Assets/BetweenWorlds/Scripts/InputReporter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+namespace Voxon.Examples.Input
+{
+	public class InputReporter : MonoBehaviour {
+
+		// Update is called once per frame
+		void Update () {
+
+			if (VXProcess.Instance.active)
+			{
+				// Left Arrow: Reduce Gravity
+				if (Voxon.Input.GetKey("ReduceGravity"))
+				{
+					ObjectSpawner.DecreaseGravity();
+				}
+
+				// Right Arrow: Increase Gravity
+				if (Voxon.Input.GetKey("IncreaseGravity"))
+				{
+					ObjectSpawner.IncreaseGravity();
+				}
+
+				// Up Arrow: Increase Spawn Rate
+				if (Voxon.Input.GetKey("IncreaseSpawnRate"))
+				{
+					ObjectSpawner.IncreaseSpawnRate();
+				}
+
+				// Down Arrow: Reduced Spawn Rate
+				if (Voxon.Input.GetKey("ReduceSpawnRate"))
+				{
+					ObjectSpawner.DecreaseSpawnRate();
+				}
+
+				if(Voxon.Input.GetKeyDown("ResetLevel"))
+				{
+					StartCoroutine(routine: LoadScene());
+				}
+			}
+		}
+
+		private IEnumerator LoadScene()
+		{
+			AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("BetweenWorlds");
+
+			// Wait until the asynchronous scene fully loads
+			while (!asyncLoad.isDone)
+			{
+				yield return null;
+			}
+		}
+	}
+}
---
+++ Assets/BetweenWorlds/Scripts/ScreenDetails.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Voxon;
+using Voxon.Examples;
+
+public class ScreenDetails : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        VXProcess.add_log_line("Between Worlds : Moving between dimensions");
+        VXProcess.add_log_line("2D->3D Demo by Ben Weatherall");
+        VXProcess.add_log_line("");
+        VXProcess.add_log_line("Controls:");
+        VXProcess.add_log_line("    Left Arrow: Reduce Gravity");
+        VXProcess.add_log_line("    Right Arrow: Increase Gravity");
+        VXProcess.add_log_line("    Up Arrow: Increase Spawn Rate");
+        VXProcess.add_log_line("    Down Arrow: Reduced Spawn Rate");
+        VXProcess.add_log_line("    Space: Reset Level");
+        VXProcess.add_log_line("Gravity (m/s): "+ Physics.gravity.y + "    Spawn Per Second: " + ((1 / Time.deltaTime) / ObjectSpawner.frameSpawn));
+    }
+}
